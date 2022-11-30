feb9e526d56196f783181538e72667dfbd0f7ecc
Ruslan Tendora <ruslantendora@gmail.com>
2022-12-01 00:38:29 +0200

    add filter

--- src/index.js
+++ src/index.js
@@ -1,34 +1,49 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import trim from 'lodash.trim';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
 
-// fetch('https://restcountries.com/v3.1/name/per')
-//   .then(r => r.json())
-//   .then(date => {
-//     date.map(c => {
-//       console.log(c.name);
-//     });
-//   });
-
+const countryList = document.querySelector('.country-list');
 const input = document.querySelector('#search-box');
+
+let markup = '';
 
 input.addEventListener('input', debounce(onInputValue, 500));
 
 function onInputValue(e) {
-  console.log(e.target.value);
-
   let name = e.target.value;
   fetchCountries(name);
 }
 
 function fetchCountries(name) {
   if (trim(name) === '') {
+    markup = '';
+    countryList.innerHTML = '';
     return;
   }
-  fetch(`https://restcountries.com/v3.1/name/${name}`)
+
+  const options = {};
+
+  fetch(
+    `https://restcountries.com/v2/name/${trim(
+      name
+    )}?fields=name,capital,population,flags,languages`
+  )
     .then(r => r.json())
-    .then(date => date.map(country => console.log(country.name)))
-    .catch(error => console.log(error));
+    .then(dates =>
+      countryList.insertAdjacentHTML('beforeend', createMarkup(dates))
+    )
+    .catch(error => Notify.failure('Oops, there is no country with that name'));
+}
+
+function createMarkup(dates) {
+  markup = dates
+    .map(
+      date =>
+        `<li class="country-item"><img src="${date.flags.svg}" width="30"><p>${date.name}</p></li>`
+    )
+    .join('');
+  return markup;
 }
