f7f04c6cf2505293a26918509da22fa6a46c91ab
Ruslan Tendora <ruslantendora@gmail.com>
2022-11-30 15:32:26 +0200

    add fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import trim from 'lodash.trim';
 
 const DEBOUNCE_DELAY = 300;
+
+// fetch('https://restcountries.com/v3.1/name/per')
+//   .then(r => r.json())
+//   .then(date => {
+//     date.map(c => {
+//       console.log(c.name);
+//     });
+//   });
+
+const input = document.querySelector('#search-box');
+
+input.addEventListener('input', debounce(onInputValue, 500));
+
+function onInputValue(e) {
+  console.log(e.target.value);
+
+  let name = e.target.value;
+  fetchCountries(name);
+}
+
+function fetchCountries(name) {
+  if (trim(name) === '') {
+    return;
+  }
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then(r => r.json())
+    .then(date => date.map(country => console.log(country.name)))
+    .catch(error => console.log(error));
+}
